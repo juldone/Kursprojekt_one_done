41157caa079c845609d78d5781e37167c314d9ba
juliandete <julian@determeyser.info>
2024-11-12 18:38:47 +0100

    armorimport & weaponimport code überarbeitet. Datenbankverbindungsfehler behoben. mongoose.connection.close aufgehoben. Weapons und Armor können dauerhaft mit get abgefragt werden.

--- backend/utils/armorimport.js
+++ backend/utils/armorimport.js
@@ -2,9 +2,9 @@ import fs from "fs";
 import path from "path";
 import mongoose from "mongoose";
 import { fileURLToPath } from "url";
-import models from "../data/armor.js"; // Importiere das gesamte Export-Objekt
+import models from "../data/armor.js"; // Importiert das gesamte Export-Objekt
 
-const { Armor } = models; // Entnimm nur das `Weapon`-Modell
+const { Armor } = models; // Entnimm nur das `Armor`-Modell
 
 // __dirname in ES-Modulen erstellen
 const __filename = fileURLToPath(import.meta.url);
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 
 export async function armorImport(req, res) {
   try {
-    // Lade die JSON-Datei aus dem angegebenen Verzeichnis
+    // JSON-Daten aus armor.json laden
     const armorData = JSON.parse(
       await fs.promises.readFile(
         path.join(__dirname, "..", "data", "armor.json"),
@@ -20,15 +20,18 @@ export async function armorImport(req, res) {
       )
     );
 
-    // Speichern der Waffen-Daten in der MongoDB
-    const docs = await Armor.insertMany(armorData);
-    console.log("Waffen erfolgreich importiert:", docs);
+    // Überprüfen, ob die Verbindung besteht
+    if (!mongoose.connection.readyState) {
+      return res.status(500).json({ error: "Keine Datenbankverbindung" });
+    }
 
-    // Verbindung schließen und Erfolgsmeldung senden
-    mongoose.connection.close();
-    res.status(200).json({ message: "Waffen erfolgreich importiert", docs });
+    // Speichern der Daten in der MongoDB
+    const docs = await Armor.insertMany(armorData);
+    console.log("Rüstungen erfolgreich importiert:", docs);
+
+    res.status(200).json({ message: "Rüstungen erfolgreich importiert", docs });
   } catch (err) {
-    console.error("Fehler beim Importieren der Waffen:", err);
-    res.status(500).json({ error: "Fehler beim Importieren der Waffen" });
+    console.error("Fehler beim Importieren der Rüstungen:", err);
+    res.status(500).json({ error: "Fehler beim Importieren der Rüstungen" });
   }
 }
--- backend/utils/weaponimport.js
+++ backend/utils/weaponimport.js
@@ -1,32 +1,15 @@
+// controllers/weaponController.js
 import fs from "fs";
 import path from "path";
-import mongoose from "mongoose";
 import { fileURLToPath } from "url";
-import models from "../data/weapons.js"; // Importiere das gesamte Export-Objekt
-import dotenv from "dotenv";
-dotenv.config();
+import models from "../data/weapons.js"; // Importiert das gesamte Export-Objekt
 
-const { Weapon } = models; // Entnimm nur das `Weapon`-Modell
-
-// __dirname in ES-Modulen erstellen
+const { Weapon } = models;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export async function weaponImport(req, res) {
   try {
-    const uri = process.env.MONGODB_URI;
-    if (!uri) {
-      console.error(
-        "MongoDB URI nicht gefunden. Bitte in der .env Datei setzen."
-      );
-      process.exit(1);
-    }
-
-    mongoose
-      .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-      .then(() => console.log("MongoDB verbunden"))
-      .catch((error) => console.error("MongoDB Verbindungsfehler:", error));
-
     // Lade die JSON-Datei aus dem angegebenen Verzeichnis
     const weaponData = JSON.parse(
       await fs.promises.readFile(
@@ -35,12 +18,11 @@ export async function weaponImport(req, res) {
       )
     );
 
-    // Speichern der Waffen-Daten in der MongoDB
+    // Daten in der MongoDB speichern
     const docs = await Weapon.insertMany(weaponData);
     console.log("Waffen erfolgreich importiert:", docs);
 
-    // Verbindung schließen und Erfolgsmeldung senden
-    mongoose.connection.close();
+    // Erfolgsmeldung senden
     res.status(200).json({ message: "Waffen erfolgreich importiert", docs });
   } catch (err) {
     console.error("Fehler beim Importieren der Waffen:", err);
