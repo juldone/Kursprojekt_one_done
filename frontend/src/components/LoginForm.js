64e1027456f79a31f6f30c6d64b53a7ecef21df2
Alf <gruber.1992@gmx.de>
2024-11-25 11:14:10 +0100

    Vorbereitet auf Weiterarbeit, Struktur mit Brian vorbereitet

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./LoginForm.css"; // Importierung der CSS Datei
 
 const LoginForm = ({ setToken, setAccountId }) => {
   const [email, setEmail] = useState("");
